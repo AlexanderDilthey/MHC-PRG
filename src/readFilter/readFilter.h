0f52bfcff93a8b9e6ffc35515152efec00c94752
Alexander Dilthey <alexanderdilthey@outlook.com>
2014-01-14 16:57:58 +0000

    Read filtering.

---
+++ src/readFilter/readFilter.h
@@ -0,0 +1,100 @@
+/*
+ * readFilter.h
+ *
+ *  Created on: 14.01.2014
+ *      Author: AlexanderDilthey
+ */
+
+#ifndef READFILTER_H_
+#define READFILTER_H_
+
+#include <string>
+#include <vector>
+#include <map>
+#include <utility>
+#include <assert.h>
+#include <functional>
+
+
+class readFilter {
+public:
+	std::string positiveFilter;
+	std::string negativeFilter;
+
+	double positiveThreshold;
+	double negativeThreshold;
+
+
+	std::string input_BAM;
+	std::string input_FASTQ;
+
+	std::string output_FASTQ;
+
+	int k;
+
+	readFilter();
+
+	void doFilter();
+};
+
+class BAMRegionSpecifier {
+public:
+	std::string ID;
+	size_t firstPos;
+	size_t lastPos;
+};
+
+class BAMalignment {
+public:
+	std::string readID;
+	std::string sequence;
+	std::string qualities;
+};
+
+class fastq_readPair {
+public:
+	bool have1;
+	bool have2;
+
+	BAMalignment a1;
+	BAMalignment a2;
+
+	fastq_readPair() : have1(false), have2(false)
+	{
+
+	}
+
+	bool takeAlignment(BAMalignment a, int which)
+	{
+		bool success = false;
+		assert((which == 1) || (which == 2));
+		if(which == 1)
+		{
+			if(have1 == false)
+			{
+				success = true;
+				a1 = a;
+			}
+		}
+		else if(which == 2)
+		{
+			if(have2 == false)
+			{
+				success = true;
+				a2 = a;
+			}
+		}
+		return success;
+	}
+
+	bool isComplete()
+	{
+		return (have1 && have2);
+	}
+};
+
+
+std::vector<BAMRegionSpecifier> getBAMregions(std::string BAMfile);
+void filterBAM(std::string BAMfile, std::string outputFile, std::function<bool(const fastq_readPair&)>* decide, std::function<void(const fastq_readPair&)>* print);
+
+#endif /* READFILTER_H_ */
