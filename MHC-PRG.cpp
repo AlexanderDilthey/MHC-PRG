d1c74214fb95be04b840a5d0a27afaab7f82516c
Alexander Dilthey <AlexanderDilthey@github.com>
2014-09-04 13:33:47 +0100

    .

--- MHC-PRG.cpp
+++ MHC-PRG.cpp
@@ -466,19 +466,19 @@ int main(int argc, char *argv[])
 		std::string starting_haplotypes_perLocus_1_str;
 		std::string starting_haplotypes_perLocus_2_str;
 
-		// HLATypeInference(input_alignedReads, graph_dir, sampleID, true, loci_string, starting_haplotypes_perLocus_1_str, starting_haplotypes_perLocus_2_str);
+		HLATypeInference(input_alignedReads, graph_dir, sampleID, true, loci_string, starting_haplotypes_perLocus_1_str, starting_haplotypes_perLocus_2_str);
 
  		// loci_string = "A";
 		// starting_haplotypes_perLocus_1_str = "A*02:07:01;A*02:265";
 		// starting_haplotypes_perLocus_2_str = "A*02:06:01";
 
- 		loci_string = "A";
-		starting_haplotypes_perLocus_1_str = "A*02:03:01;A*02:264";
-		starting_haplotypes_perLocus_2_str = "A*24:02:01:01;A*24:02:01:02L;A*24:02:03Q;A*24:02:10;A*24:09N;A*24:11N";
+ 		// loci_string = "A";
+		// starting_haplotypes_perLocus_1_str = "A*02:03:01;A*02:264";
+		// starting_haplotypes_perLocus_2_str = "A*24:02:01:01;A*24:02:01:02L;A*24:02:03Q;A*24:02:10;A*24:09N;A*24:11N";
 			
 		HLAHaplotypeInference(input_alignedReads, graph_dir, sampleID, loci_string, starting_haplotypes_perLocus_1_str, starting_haplotypes_perLocus_2_str);
 		
-		// HLATypeInference(input_alignedReads, graph_dir, sampleID, false, loci_string, starting_haplotypes_perLocus_1_str, starting_haplotypes_perLocus_2_str);
+		HLATypeInference(input_alignedReads, graph_dir, sampleID, false, loci_string, starting_haplotypes_perLocus_1_str, starting_haplotypes_perLocus_2_str);
 		
 	}
 	else if((arguments.size() > 0) && (arguments.at(1) == "nextGenContigValidation"))
